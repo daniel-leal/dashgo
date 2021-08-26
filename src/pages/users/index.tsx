fdadc13e69d6d8acf7263e687dad2e16b1c71051
Daniel Leal <danielleal94@gmail.com>
2021-08-26 19:46:42 -0300

    React-query e componente de paginacao

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -13,7 +13,8 @@ import {
   Checkbox,
   Td,
   Text,
-  useBreakpointValue
+  useBreakpointValue,
+  Spinner
 } from '@chakra-ui/react'
 import { RiAddLine, RiPencilLine } from 'react-icons/ri'
 
@@ -21,8 +22,11 @@ import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
 import { Sidebar } from '../../components/Sidebar'
 import Link from 'next/link'
+import { useUsers } from '../../services/hooks/useUsers'
 
 export default function UserList(): JSX.Element {
+  const { data, isLoading, isFetching, error } = useUsers()
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
@@ -39,6 +43,9 @@ export default function UserList(): JSX.Element {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
 
             <Link href="/users/create" passHref>
@@ -54,98 +61,67 @@ export default function UserList(): JSX.Element {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={['4', '4', '6']} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuario</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-                <Th width="8"></Th>
-              </Tr>
-            </Thead>
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={['4', '4', '6']} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuario</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                    <Th width="8"></Th>
+                  </Tr>
+                </Thead>
 
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Daniel Leal</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      danielleal94@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Daniel Leal</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      danielleal94@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Daniel Leal</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      danielleal94@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
+                <Tbody>
+                  {data.map(user => (
+                    <Tr key={user.id}>
+                      <Td px={['4', '4', '6']}>
+                        <Checkbox colorScheme="pink" />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight="bold">{user.name}</Text>
+                          <Text fontSize="sm" color="gray.300">
+                            {user.email}
+                          </Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && <Td>{user.createdAt}</Td>}
+                      <Td>
+                        <Button
+                          as="a"
+                          size="sm"
+                          fontSize="sm"
+                          colorScheme="purple"
+                          leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                        >
+                          Editar
+                        </Button>
+                      </Td>
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
 
-          <Pagination />
+              <Pagination
+                totalCountOfRegisters={200}
+                currentPage={5}
+                // eslint-disable-next-line @typescript-eslint/no-empty-function
+                onPageChange={() => {}}
+              />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
---
+++ src/services/mirage/index.ts
@@ -0,0 +1,63 @@
+import { createServer, Factory, Model, Response } from 'miragejs'
+import faker from 'faker'
+
+type User = {
+  name: string
+  email: string
+  createdAt: string
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export function makeServer() {
+  const server = createServer({
+    models: {
+      user: Model.extend<Partial<User>>({})
+    },
+
+    factories: {
+      user: Factory.extend({
+        name() {
+          return faker.name.findName()
+        },
+        email() {
+          return faker.internet.email().toLowerCase()
+        },
+        createdAt() {
+          return faker.date.recent(10)
+        }
+      })
+    },
+
+    seeds(server) {
+      server.createList('user', 200)
+    },
+
+    routes() {
+      this.namespace = 'api'
+      this.timing = 750
+
+      this.get('/users', function (schema, request) {
+        const { page = 1, perPage = 10 } = request.queryParams
+
+        const total = schema.all('user').length
+
+        const pageStart = (Number(page) - 1) * Number(perPage)
+        const pageEnd = pageStart + Number(perPage)
+
+        const users = this.serialize(schema.all('user')).users.slice(
+          pageStart,
+          pageEnd
+        )
+
+        return new Response(200, { 'x-total-count': String(total) }, { users })
+      })
+
+      this.post('/users')
+
+      this.namespace = ''
+      this.passthrough()
+    }
+  })
+
+  return server
+}
